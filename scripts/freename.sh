3bcfc6cd28fb1ca24a29930ca356350e399e86d1
Michael Myer <misc.myer@zoho.com>
2020-01-10 11:12:11 +0100

    freename

--- scripts/freename.sh
+++ scripts/freename.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ ! $1 ]; then
 		echo "Usage: 
-$0 [path]filename [filenamepart2]
+$0 [path]filename [filenamepart2] [max]
 
 Looks for the first number for a filename composed by arg1\"number\"arg2,
-that doesn't exist.
+that doesn't exist. max can be at maximum 999 (default)
 Prints the name to stdout 
 
 example: echo test > \`freename.sh test .txt\`
@@ -15,12 +15,20 @@ example: echo test > \`freename.sh test .txt\`
 fi
 
 i=1
-echo $1$i$2
-while [ -e $1$i$2 ] ;do
-		echo $1$i$2
-		i=[ i+1 ]
+max=$3
+if [ ! $3 ];then 
+		max=999
+fi
+
+while [ -e "$1$i$2" ] ;do
+		i=$[ i+1 ]
+		if [[ $i > $max ]]
+		then
+				echo $0 error: max $max reached >&2
+				exit 2
+		fi
 done
 
-
+echo $1$i$2
 
 
