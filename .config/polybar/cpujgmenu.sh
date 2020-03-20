425a996009e6a062bc8826994b5c252522beacec
Michael Myer <misc.myer@zoho.com>
2020-03-20 13:17:25 +0100

    cpujgmenu polybar script

---
+++ .config/polybar/cpujgmenu.sh
@@ -0,0 +1,1 @@
+ps axo rsz,pid,pcpu,user,cmd O-C --no-heading | sed -n -e 10q -e "s/^\(\S*\s*\)\(\S*\)\s*\(\S*\s*\S*\s*\S*\).*/\1 \2 \3,sh ~\/.config\/polybar\/dzenprocinfo.sh \2/p" | jgmenu --vsimple --at-pointer
