38b50a712096fd06d7ddccdb850e6f4ad88e141f
Michael Myer <misc.myer@zoho.com>
2020-04-23 14:23:07 +0200

    Thu Apr 23 14:23:06 CEST 2020

---
+++ scripts/set-gcc.sh
@@ -0,0 +1,5 @@
+alias make=make
+export CC=gcc
+export LDFLAGS=""
+export CFLAGS="-O2 -s"
+alias gcc=gcc
---
+++ scripts/set-musl.sh
@@ -0,0 +1,5 @@
+alias make=make
+export CC=musl-gcc
+export LDFLAGS="-static"
+export CFLAGS="-O2 -s"
+alias gcc=musl-gcc
