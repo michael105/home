af04a4c3c1d4bc47d92ea52966b64b5f0608fd03
Michael Myer <misc.myer@zoho.com>
2020-04-30 04:03:59 +0200

    Thu Apr 30 04:03:59 CEST 2020

--- .config/i3/scripts/alsa_volup.sh
+++ .config/i3/scripts/alsa_volup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if [ -z $1 ]; then
+		vol=1
+else
+		vol=$1
+fi
 
-amixer sset Master,0 $((`amixer sget Master,0 | tail -1 | sed -E 's/.*Playback (..).*/\1/'` + 1 ))
+amixer sset Master,0 $((`amixer sget Master,0 | tail -1 | sed -E 's/.*Playback (..).*/\1/'` + $vol ))
 
