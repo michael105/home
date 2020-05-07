b5bb7502c31fceca0d85028b077b3f0dcc23fd7f
Michael Myer <misc.myer@zoho.com>
2020-05-07 19:31:56 +0200

    (minor) Thu May  7 19:31:56 CEST 2020

--- .config/i3/scripts/compilehelp.pl
+++ .config/i3/scripts/compilehelp.pl
@@ -4,6 +4,13 @@
 
 $posrightcol = 700;
 $marginleft = 40;
+$fg = "white";
+
+my $pattern = { 
+		'#'=> "^bg(orange)^fg(black)",
+		'##'=> "^fg(blue)" };
+
+my @pm = keys(%{$pattern});
 
 my $fn = shift or die;
 
@@ -15,14 +22,25 @@ my @f = <F>;
 my $pos="";
 my $rc = 0;
 my $ln = 0;
+my $tableline = 0;
 my @o;
 
 foreach my $l ( @f ){
-		next if ( $l =~/^#leftcol/ );
-		if ( $l =~ /^#rightcol/ ){
-				$rc = 1;
+		if ( $l =~ /^#\+table/ ){
+				$tableline = $ln;
 				next;
 		}
+		next if ( $l =~/^#\+leftcol/ );
+		if ( $l =~ /^#\+rightcol/ ){
+				$rc = $tableline;
+				next;
+		}
+		if ( $l =~/^#/ ){
+				$l =~ s/^(#*) (.*)/$pattern->{$1}$2^fg($fg)^bg()/;
+		} else {
+				$l =~ s/^/^fg($fg)/;
+		}
+
 		if ( $rc > 0 ){
 				$o[$rc] =~ s/\n$/^pa($posrightcol)$l/;
 				$rc++;
--- .config/i3/scripts/help.sh
+++ .config/i3/scripts/help.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 
-OPTS='-bg #111111 -fg grey70 -p -l 30 -e onstart=uncollapse,scrollhome,grabkeys;button5=scrolldown;key_Down=scrolldown;button4=scrollup;key_Up=scrollup;key_Page_Down=scrolldown:30;key_Page_Up=scrollup:30;key_Escape=exit;button3=exit;entertitle=grabkeys;enterslave=grabkeys'
+OPTS='-bg #111111 -fg white -p -l 30 -e onstart=uncollapse,scrollhome,grabkeys;button5=scrolldown;key_Down=scrolldown;button4=scrollup;key_Up=scrollup;key_Page_Down=scrolldown:30;key_Page_Up=scrollup:30;key_Escape=exit;button3=exit;entertitle=grabkeys;enterslave=grabkeys'
 
-(echo "^fg(#323232)^bg(#5FBF77) Documentation (use: scrollwheel, arrow keys, PgUP/PgDown to scroll. Escape or right mouse button to quit) "; cat $HOME/.config/i3/scripts/help.txt.dzen) | dzen2 $OPTS
+(echo "^fg(#000000)^bg(#004AB7) i3 keys (scrollwheel, arrow keys, PgUP/PgDown scroll; Esc or right mouse button quit) "; cat $HOME/.config/i3/scripts/help.txt.dzen) | dzen2 $OPTS
 
